0b2b98ce7deb9f5ba6fc296bbbabd1f8b926c08e
Ngogaserge <sergengoga05@gmail.com>
2025-03-04 02:12:27 +0200

    refractor: changed the alert message

--- public/script.js
+++ public/script.js
@@ -19,7 +19,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
   
       const result = await response.json();
       if (response.ok) {
-        alert('Message sent! I Will get back to you shortly');
+        alert('Message sent! I Will get back to you soon');
       } else {
         alert(`Failed to send message: ${result.error}`);
       }
